570b6576d088760c4fff2625ae01434fd3f3fefa
muhammadd03 <muhammad.03kh@gmail.com>
2026-01-06 21:51:33 +0500

    Test 3

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = BASE_DIR / "staticfiles"
+# STATICFILES_DIRS = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
